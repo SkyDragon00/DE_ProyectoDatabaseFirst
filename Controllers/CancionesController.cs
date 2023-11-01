2d2c3f94e3c4a30f719cd5820635c26650204eb2
Domenica Escobar <domenica.escobar.moyano@udla.edu.ec>
2023-11-01 13:13:56 -0600

    Se agregaron el resto de controladores

---
+++ Controllers/CancionesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DE_ProyectoDF.Models;
+
+namespace DE_ProyectoDF.Controllers
+{
+    public class CancionesController : Controller
+    {
+        private CancionesDFEntities db = new CancionesDFEntities();
+
+        // GET: Canciones
+        public ActionResult Index()
+        {
+            var cancion = db.Cancion.Include(c => c.Album);
+            return View(cancion.ToList());
+        }
+
+        // GET: Canciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cancion cancion = db.Cancion.Find(id);
+            if (cancion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cancion);
+        }
+
+        // GET: Canciones/Create
+        public ActionResult Create()
+        {
+            ViewBag.AlbumID = new SelectList(db.Album, "AlbumID", "NombreAlbum");
+            return View();
+        }
+
+        // POST: Canciones/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CancionID,NombreCancion,Duracion,AnioLanzamiento,Compositor,AlbumID")] Cancion cancion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cancion.Add(cancion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AlbumID = new SelectList(db.Album, "AlbumID", "NombreAlbum", cancion.AlbumID);
+            return View(cancion);
+        }
+
+        // GET: Canciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cancion cancion = db.Cancion.Find(id);
+            if (cancion == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AlbumID = new SelectList(db.Album, "AlbumID", "NombreAlbum", cancion.AlbumID);
+            return View(cancion);
+        }
+
+        // POST: Canciones/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CancionID,NombreCancion,Duracion,AnioLanzamiento,Compositor,AlbumID")] Cancion cancion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cancion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AlbumID = new SelectList(db.Album, "AlbumID", "NombreAlbum", cancion.AlbumID);
+            return View(cancion);
+        }
+
+        // GET: Canciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cancion cancion = db.Cancion.Find(id);
+            if (cancion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cancion);
+        }
+
+        // POST: Canciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cancion cancion = db.Cancion.Find(id);
+            db.Cancion.Remove(cancion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
